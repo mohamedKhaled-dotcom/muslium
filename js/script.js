0453fff00a7aab589d49d71c90be79e89c824b94
mohamedKhaled-dotcom <mokhaled975140@gmail.com>
2024-11-18 14:03:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let container = document.getElementById("container");
+if (container) {
+  setTimeout(function() {
+    container.style.backgroundImage = 'linear-gradient(to right, #ff07072e, #ff99004d), url("bgContainer.jpg")';
+  }, 2000);
+}
